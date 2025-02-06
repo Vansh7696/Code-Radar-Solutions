79a912de38d5b67c09f12fb84e0218acf5028dd1
Vansh7696 <studiousvansh@gmail.com>
2025-02-06 17:24:10 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,10 +2,6 @@
 int main() {
     int num1, num2;
     char operator;
-    if (scanf("%d %d %c", &num1, &num2, &operator) != 3) {
-        printf("Error");
-        return 1;
-    }
     if (operator == '+') {
         printf("%d\n", num1 + num2);
     } else if (operator == '-') {
