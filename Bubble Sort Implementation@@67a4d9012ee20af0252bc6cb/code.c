51993cb2fd52c52f80f6da21104e6c6fcd42519c
Vansh7696 <studiousvansh@gmail.com>
2025-04-08 09:31:57 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,30 +1,47 @@
 #include <stdio.h>
+
+// Function to perform Bubble Sort
 void bubbleSort(int arr[], int n) {
+    int temp;
     for (int i = 0; i < n - 1; i++) {
+        // Flag to check if any swapping occurred
+        int swapped = 0;
+
         for (int j = 0; j < n - i - 1; j++) {
+            // Compare adjacent elements
             if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
+                // Swap if elements are in wrong order
+                temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                swapped = 1;
             }
         }
+
+        // If no two elements were swapped in the inner loop, array is sorted
+        if (!swapped)
+            break;
     }
 }
+
+// Function to print the array
 void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
-    }
     printf("\n");
 }
-int main(){
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+
+// Main function
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: \n");
+    printArray(arr, n);
 
     bubbleSort(arr, n);
+
+    printf("Sorted array: \n");
     printArray(arr, n);
 
     return 0;
